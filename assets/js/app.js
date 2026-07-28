9974e0b3eb87795287cfdcfb5e6c75483e4b7c08
raf003771-netizen <raf003771@gmail.com>
2026-07-29 03:04:16 +0400

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-(()=>{
-  BusApp.mapApi.init();
-  BusApp.ui.bind();
-  BusApp.mapApi.map.on('load',()=>{BusApp.ui.bindMap();BusApp.stopBoard.bind();});
-  BusApp.routes.initDb();
-  if('serviceWorker' in navigator) window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js').catch(console.error));
-})();
