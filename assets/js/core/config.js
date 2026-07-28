fe4165d057c81cf6fdd441a50b71b568708f72ac
raf003771-netizen <raf003771@gmail.com>
2026-07-29 02:41:35 +0400

    Delete assets directory

--- assets/js/core/config.js
+++
@@ -1,8 +0,0 @@
-window.BusApp = window.BusApp || {};
-BusApp.config = {
-  API_BASE: 'https://bus-proxy.raf003771.workers.dev',
-  MAP_CENTER: [41.63, 41.631],
-  MAP_ZOOM: 14,
-  BUS_REFRESH_MS: 4000,
-  OSM_TILES: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-};
